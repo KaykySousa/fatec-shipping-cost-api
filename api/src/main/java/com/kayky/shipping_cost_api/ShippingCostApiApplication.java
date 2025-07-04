49c8623544be09af4d23278ca9c7294acfe0a1b1
KaykySousa <kayky.sousa.dev@gmail.com>
2025-07-04 00:03:48 -0300

    initial commit

---
+++ api/src/main/java/com/kayky/shipping_cost_api/ShippingCostApiApplication.java
@@ -0,0 +1,13 @@
+package com.kayky.shipping_cost_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingCostApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingCostApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/kayky/shipping_cost_api/ShippingCostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayky.shipping_cost_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingCostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
